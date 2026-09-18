8c89dd5e52080f5c0d7662e0e9e7c873529e598d
4rd25ggngd-dot <4rd25ggngd@privaterelay.appleid.com>
2026-09-19 07:17:22 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,7 @@
-const CACHE='asset-manager-v3';
+const C='asset-manager-stable-v4';
 const STATIC=['./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)));self.skipWaiting();});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{
- if(e.request.method!=='GET')return;
- const u=new URL(e.request.url);
- if(u.pathname.endsWith('/')||u.pathname.endsWith('/index.html')){
-   e.respondWith(fetch(e.request,{cache:'no-store'}).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put('./index.html',c));return r;}).catch(()=>caches.match('./index.html')));
- }else{
-   e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
- }
-});
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(STATIC)));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(a=>Promise.all(a.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;const u=new URL(e.request.url);
+if(u.pathname.endsWith('/')||u.pathname.endsWith('/index.html'))e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match('./index.html')));
+else e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
